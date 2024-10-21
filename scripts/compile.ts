1fd778bd4c7c6630cd846a80f8b9bcc93c83a24a
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-10-21 18:11:40 +0200

    whoops

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -1,17 +1,17 @@
 // TODO: refactor this piece of garbage so this code isn't trash
-import { APP_NAME } from "../src/constants.ts";
+import { APP_NAME, VERSION } from "../src/constants.ts";
 
 const PERMISSIONS_NEEDED = ["--allow-read", "--allow-write", "--allow-net", "--allow-env", "--allow-run"];
 const TARGETS = {
     win64: ["x86_64-pc-windows-msvc", "win64"],
-    darwinArm: ["aarch64-apple-darwin", "macOs_rm"],
+    darwinArm: ["aarch64-apple-darwin", "macOs_arm"],
     linuxArm: ["aarch64-unknown-linux-gnu", "linux_arm"],
     darwin64: ["x86_64-apple-darwin", "macOs_x86_64"],
     linux64: ["x86_64-unknown-linux-gnu", "linux_x86_64"],
 };
 
 const ALL_COMMANDS = Object.entries(TARGETS).map(([_key, [target, output]]) => {
-    const compiledName = target![1] === "win64" ? `${APP_NAME.CASED}-${output}.exe` : `${APP_NAME.CASED}-${output}`;
+    const compiledName = target![1] === "win64" ? `${APP_NAME.CASED}-${VERSION}-${output}.exe` : `${APP_NAME.CASED}-${VERSION}-${output}`;
 
     const args = [
         "compile",
