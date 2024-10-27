e838766d8d24494856aa96bed985685b4c7a5512
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-10-27 10:32:27 +0100

    made config paths lowercase

--- src/functions/config.ts
+++ src/functions/config.ts
@@ -20,10 +20,10 @@ export async function GetAppPath(
         Deno.exit(1);
     }
 
-    const BASE_DIR = await JoinPaths(appDataPath, APP_NAME.CASED);
-    const PROJECTS = await JoinPaths(BASE_DIR, `${APP_NAME.CASED}-${I_LIKE_JS.MFS.toLowerCase().replace("*", "o").replace("*", "u")}.txt`);
-    const LOGS = await JoinPaths(BASE_DIR, `${APP_NAME.CASED}-logs.log`);
-    const UPDATES = await JoinPaths(BASE_DIR, `${APP_NAME.CASED}-updates.json`);
+    const BASE_DIR = await JoinPaths(appDataPath, APP_NAME.CLI);
+    const PROJECTS = await JoinPaths(BASE_DIR, `${APP_NAME.CLI}-${I_LIKE_JS.MFS.toLowerCase().replace("*", "o").replace("*", "u")}.txt`);
+    const LOGS = await JoinPaths(BASE_DIR, `${APP_NAME.CLI}-logs.log`);
+    const UPDATES = await JoinPaths(BASE_DIR, `${APP_NAME.CLI}-updates.json`);
 
     switch (path) {
         case "BASE":
@@ -50,12 +50,14 @@ export async function FreshSetup(): Promise<void> {
     try {
         await Deno.mkdir(await GetAppPath("BASE"), { recursive: true });
 
-        if (!(await CheckForPath(await GetAppPath("MOTHERFKRS")))) {
-            await Deno.writeTextFile(await GetAppPath("MOTHERFKRS"), "");
+        const projectPath = await GetAppPath("MOTHERFKRS");
+        if (!(await CheckForPath(projectPath))) {
+            await Deno.writeTextFile(projectPath, "");
         }
 
-        if (!(await CheckForPath(await GetAppPath("LOGS")))) {
-            await Deno.writeTextFile(await GetAppPath("LOGS"), "");
+        const logsPath = await GetAppPath("LOGS");
+        if (!(await CheckForPath(logsPath))) {
+            await Deno.writeTextFile(logsPath, "");
         }
     } catch (error) {
         await LogStuff(`Some ${I_LIKE_JS.MFN} error happened trying to setup config files: ${error}`, "error");
