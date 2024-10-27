bb0ade8eed22f973176503e78ed3c0b31ab608bb
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-10-27 11:55:29 +0100

    make Commander sync a bit better

--- src/functions/cli.ts
+++ src/functions/cli.ts
@@ -23,26 +23,47 @@ export interface CommanderOutput {
 
 /**
  * Executes commands and automatically handles errors.
+ * 
+ * Also, it _tries_ to log their content so they look "real" - that still has some known issues. But it works for now, so there's no reason to touch it.
  *
  * @export
  * @async
  * @param {string} main Main command.
  * @param {string[]} stuff Additional args for the command.
  * @param {boolean} log Whether to `LogStuff` the `stdout` or the `stderr`.
- * @returns {CommanderOutput} An object with a boolean telling if it was successful and it's stdout (or stderr).
+ * @returns {Promise<CommanderOutput>} An object with a boolean telling if it was successful and its output.
  */
 export async function Commander(main: string, stuff: string[], log: boolean): Promise<CommanderOutput> {
     const decoder = new TextDecoder();
+    const encoder = new TextEncoder()
     const command = new Deno.Command(main, {
         args: stuff,
+        stdout: "piped",
+        stderr: "piped",
     });
-    const output = await command.output();
+
+    const process = command.spawn();
+
+    for await (const chunk of process.stdout) {
+        const output = decoder.decode(chunk);
+        Deno.stdout.writeSync(encoder.encode("\x1b[2K\r")); // this should clean the stdout
+        Deno.stdout.writeSync(encoder.encode(output));
+    }
+
+    for await (const chunk of process.stderr) {
+        const errorOutput = decoder.decode(chunk);
+        Deno.stderr.writeSync(encoder.encode("\x1b[2K\r")); // this should clean the stderr
+        Deno.stderr.writeSync(encoder.encode(errorOutput));
+    }
+
+    const output = await process.output();
 
     if (!output.success) {
         if (!log) Deno.exit(1);
-        await LogStuff(
-            `An error happened with ${main} ${stuff.toString()}: ` + output.stderr ? decoder.decode(output.stderr) : "(Error is unknown)",
-        );
+
+        const errorMessage = output.stderr ? decoder.decode(output.stderr) : "(Error is unknown)";
+
+        await LogStuff(`An error happened with ${main} ${stuff.join(" ")}: ${errorMessage}`);
         Deno.exit(1);
     }
 
