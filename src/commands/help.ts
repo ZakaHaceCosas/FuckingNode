56a64939757a077878456f9767690ab53b7e796f
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-11-01 11:41:36 +0100

    update help menu

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -7,8 +7,8 @@ function joinObjectValues(obj: Record<string, string>): string {
 
 export default async function TheHelper(query?: "manager" | "clean" | "settings" | string) {
     const _USAGE = {
-        clean: SpaceString("clean   <intensity> [--update] [--verbose]", 8),
-        manager: SpaceString("manager add <path> | remove <path> | ignore <path> | revive <path> | list | cleanup", 8),
+        clean: SpaceString("clean    <intensity> [--update] [--verbose]", 8),
+        manager: SpaceString("manager  add <path> | remove <path> | ignore <path> | revive <path> | list | cleanup", 8),
         stats: SpaceString("stats", 8),
         migrate: SpaceString("migrate", 8),
         self_update: SpaceString("self-update", 8),
@@ -17,30 +17,41 @@ export default async function TheHelper(query?: "manager" | "clean" | "settings"
     };
     const USAGE = joinObjectValues(_USAGE);
     const _OPTIONS = {
-        version: SpaceString(`--version       Show the version of ${APP_NAME.STYLED} you're currently on.`, 4),
+        version: SpaceString(
+            `--version                                                  Show the version of ${APP_NAME.STYLED} you're currently on.`,
+            4,
+        ),
     };
     const OPTIONS = joinObjectValues(_OPTIONS);
     const _CLEAN_OPTIONS = {
         intensity: SpaceString(
-            "<intensity>     Can be 'normal' (default), 'hard', or 'maxim'. The higher, the deeper (but more time-consuming) the cleaning will be.",
+            "<intensity>                                                Can be 'normal' (default), 'hard', or 'maxim'. The higher, the deeper (but more time-consuming) the cleaning will be.",
             4,
         ),
-        update: SpaceString("--update        Update all your projects before cleaning them.", 4),
-        verbose: SpaceString("--verbose       Show more detailed ('verbose') logs.", 4),
+        update: SpaceString("--update                                                   Update all your projects before cleaning them.", 4),
+        verbose: SpaceString("--verbose                                                  Show more detailed ('verbose') logs.", 4),
     };
     const _MANAGER_OPTIONS = {
-        add: SpaceString("add <path>      Adds a project to your list.", 4),
-        remove: SpaceString("remove <path>   Removes a project from your list.", 4),
-        ignore: SpaceString("ignore <path>   Ignores a project so it's not cleaned or updated but still on your list.", 4),
-        revive: SpaceString("revive <path>   Stops ignoring an ignored project.", 4),
-        list: SpaceString("list            Lists all of your added projects.", 4),
+        add: SpaceString("add <path>                                                 Adds a project to your list.", 4),
+        remove: SpaceString("remove <path>                                              Removes a project from your list.", 4),
+        ignore: SpaceString(
+            "ignore <path>                                              Ignores a project so it's not cleaned or updated but still on your list.",
+            4,
+        ),
+        revive: SpaceString("revive <path>                                              Stops ignoring an ignored project.", 4),
+        list: SpaceString("list                                                       Lists all of your added projects.", 4),
         cleanup: SpaceString(
-            "cleanup         Shows projects that aren't valid (invalid path, no package.json, duplicates...) and lets you remove them.",
+            "cleanup                                                    Shows invalid (invalid path, duplicates...) and lets you remove them.",
             4,
         ),
     };
     const MANAGER_OPTIONS = joinObjectValues(_MANAGER_OPTIONS);
     const CLEAN_OPTIONS = joinObjectValues(_CLEAN_OPTIONS);
+    const _SETTINGS = {
+        flush: SpaceString(`flush <"logs" | "updates" | "projects" | "all"> [--force]  Flushes (removes) chosen config files.`, 4),
+        schedule: SpaceString(`schedule <hour> <day>                                      Schedule ${APP_NAME.STYLED}'s cleanup.`, 4),
+    };
+    const SETTINGS = joinObjectValues(_SETTINGS);
 
     const FULL_HELP = `Usage: ${APP_NAME.CLI}\n` +
         USAGE +
@@ -48,10 +59,12 @@ export default async function TheHelper(query?: "manager" | "clean" | "settings"
         CLEAN_OPTIONS +
         "\n\n'manager' will let you manage projects. Flags:\n" +
         MANAGER_OPTIONS +
+        "\n\n'settings' allows you to tweak some parts of the CLI. Flags:\n" +
+        SETTINGS +
         "\n\nAdditional flags:\n" +
         OPTIONS +
         "\n\nAdditional commands:\n" +
-        SpaceString("self-update     Checks the GitHub repo for updates.", 4);
+        SpaceString("self-update      Checks the GitHub repo for updates.", 4);
 
     if (!query) {
         await LogStuff(
@@ -79,11 +92,12 @@ export default async function TheHelper(query?: "manager" | "clean" | "settings"
             break;
         case "settings":
             await LogStuff(
-                `Currently supported settings:\nschedule <hour> <day>                                       Schedule ${APP_NAME.STYLED} to run periodically.\nflush <"logs" | "updates" | "projects" | "all"> [--force]   Removes log files, update cache, project list, or everything.`,
+                `Currently supported settings:\n`,
                 undefined,
                 true,
             );
             break;
+        case "":
         default:
             await LogStuff(
                 FULL_HELP,
