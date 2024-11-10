34197bb9090e5a7c6f1fb1a9f514400666d639a3
ZakaHaceCosas <zakahacecosas@protonmail.com>
2024-11-10 13:46:20 +0100

    hard cleaner: add tmp dir to fix Bun + better logging

--- src/commands/toolkit/cleaner.ts
+++ src/commands/toolkit/cleaner.ts
@@ -1,7 +1,7 @@
 import { I_LIKE_JS } from "../../constants.ts";
 import { Commander, CommandExists } from "../../functions/cli.ts";
 import { CheckForPath, JoinPaths, ParsePath } from "../../functions/filesystem.ts";
-import { LogStuff } from "../../functions/io.ts";
+import { ColorString, LogStuff } from "../../functions/io.ts";
 import { SUPPORTED_NODE_LOCKFILE } from "../../types.ts";
 
 export async function PerformNodeCleaning(
@@ -82,11 +82,18 @@ export async function PerformNodeCleaning(
 
 // cache cleaning is global, so doing these for every project like we used to do
 // is a waste of resources
-export async function PerformHardCleanup() {
+export async function PerformHardCleanup(): Promise<void> {
     await LogStuff(
-        "Time for hard-pruning! Wait patiently, please (caches take a while to clean)",
+        ColorString("Time for hard-pruning! Wait patiently, please (caches can take a while to clean).", "italic"),
         "package",
     );
+
+    const tmp = await Deno.makeTempDir({
+        prefix: "FKNODE-HARD-CLEAN-TMP",
+    }); // we make a temporal dir where we'll do "placebo" inits, as bun requires you to be in a bun project for it to clean stuff
+    // for deno idk if its necessary but i'll do it anyway
+
+    Deno.chdir(tmp); // assuming this is called from the main cleaner, which at the end returns to the DIR it should.
 
     const npmHardPruneArgs: string[] = ["cache", "clean", "--force"];
     const pnpmHardPruneArgs: string[] = ["store", "prune"];
@@ -97,11 +104,41 @@ export async function PerformHardCleanup() {
     const bunHardPruneArgs: string[] = ["pm", "cache", "rm"];
     // const denoHardPruneArgs: string[] = ["clean"];
 
-    if (await CommandExists("npm")) await Commander("npm", npmHardPruneArgs);
-    if (await CommandExists("pnpm")) await Commander("pnpm", pnpmHardPruneArgs);
-    if (await CommandExists("yarn")) await Commander("yarn", yarnHardPruneArgs);
-    if (await CommandExists("bun")) await Commander("bun", bunHardPruneArgs);
-    // if (await CommandExists("deno")) await Commander("deno", denoHardPruneArgs);
+    await LogStuff(
+        ColorString("NPM", "red"),
+        "package",
+    );
+    if (await CommandExists("npm")) await Commander("npm", npmHardPruneArgs, true);
+    await LogStuff("Done", "tick");
+    await LogStuff(
+        ColorString("PNPM", "bright-yellow"),
+        "package",
+    );
+    if (await CommandExists("pnpm")) await Commander("pnpm", pnpmHardPruneArgs, true);
+    await LogStuff("Done", "tick");
+    await LogStuff(
+        ColorString("YARN", "purple"),
+        "package",
+    );
+    if (await CommandExists("yarn")) await Commander("yarn", yarnHardPruneArgs, true);
+    await LogStuff("Done", "tick");
+    await LogStuff(
+        ColorString("BUN", "pink"),
+        "package",
+    );
+    if (await CommandExists("bun")) {
+        await Commander("bun", ["init", "-y"], false); // placebo
+        await Commander("bun", bunHardPruneArgs, true);
+    }
+    await LogStuff("Done", "tick");
+    await LogStuff(
+        ColorString("DENO", "bright-blue"),
+        "package",
+    );
+    /* if (await CommandExists("deno")) {
+        await Commander("deno", ["init"], false); // placebo 2
+        await Commander("deno", denoHardPruneArgs, true);
+    } */
 
     // deno requires this glue fix
     // because apparently i cannot clear the cache of deno
@@ -113,15 +150,23 @@ export async function PerformHardCleanup() {
             const denoDir: string | undefined = Deno.env.get("DENO_DIR");
             if (!denoDir) throw "lmao";
             await Deno.remove(denoDir);
+            await LogStuff("Done", "tick")
             // the CLI calls this kind of behaviors "maxim" cleanup
             // yet we're doing from the "hard" preset and not the
             // "maxim" one
             // epic.
         } catch {
+            return;
             // nothing happened.
             // i don't know what could happen if i delete the cache *live*
             // shouldn't be an issue as the compiled executable brings it's own
             // deno, but well, lets try :]
         }
     }
+
+    await Deno.remove(tmp, {
+        recursive: true
+    }); // free the user's space
+
+    return;
 }
