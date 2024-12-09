2a93f9228b7e9e9a297cfdb92df01c8d3f08c0af
Zaka <zakahacecosas@protonmail.com>
2024-12-09 19:25:55 +0100

    another test

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,15 +1,8 @@
-import type { tURL } from "../types/misc.ts";
-
-/**
- * Fetches a resource using GitHub's headers.
- *
- * @export
- * @async
- * @param {tURL} url
- * @returns {Promise<Response>}
- */
-export async function FetchGitHub(url: tURL): Promise<Response> {
-    return await fetch(url, {
-        headers: { Accept: "application/vnd.github.v3+json" },
-    });
-}
+import type{tURL}from"../types/misc.ts";/**
+* Fetches a resource using GitHub's headers.
+*
+* @export
+* @async
+* @param{tURL}url
+* @returns{Promise<Response>}
+*/export async function FetchGitHub(url:tURL):Promise<Response>{return await fetch(url,{headers:{Accept:"application/vnd.github.v3+json"},});}
