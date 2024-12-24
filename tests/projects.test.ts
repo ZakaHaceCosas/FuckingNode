61ce89f5ca6cc918adb9b492c0c1fc7b112028fc
Zaka <zakahacecosas@protonmail.com>
2024-12-24 16:37:38 +0100

    update test

--- tests/projects.test.ts
+++ tests/projects.test.ts
@@ -1,23 +1,28 @@
 import { GetProjectEnvironment } from "../src/functions/projects.ts";
 import { assertEquals } from "@std/assert";
-import { APP_NAME } from "../src/constants.ts";
 import type { ProjectEnv } from "../src/types/runtimes.ts";
+import { JoinPaths, ParsePath } from "../src/functions/filesystem.ts";
+
+const CONSTANTS = {
+    CWD: Deno.cwd(),
+    ENV_PATH: await JoinPaths(Deno.cwd(), "tests/environment"),
+}
 
 // naming things is fr the hardest
-// TODO - im stupid people cant just clone this repo and run the tests (they depend on my local file structure)
 const uwu: ProjectEnv = {
-    main: `C:\\Users\\Zaka\\${APP_NAME.CASED}\\real-life-tests\\uwu\\package.json`,
+    main: await ParsePath(`${CONSTANTS.ENV_PATH}/uwu/package.json`),
     runtime: "node",
     manager: "npm",
-    lockfile: `C:\\Users\\Zaka\\${APP_NAME.CASED}\\real-life-tests\\uwu\\package-lock.json`,
-    hall_of_trash: `C:\\Users\\Zaka\\${APP_NAME.CASED}\\real-life-tests\\uwu\\node_modules`,
+    lockfile: await ParsePath(`${CONSTANTS.ENV_PATH}/uwu/package-lock.json`),
+    hall_of_trash: await ParsePath(`${CONSTANTS.ENV_PATH}/uwu/node_modules`),
 };
 
 Deno.test(
     {
         name: "reads node env",
         fn: async () => {
-            const env = await GetProjectEnvironment("./real-life-tests/uwu");
+            console.log("Looking for", uwu.main, "environment.");
+            const env = await GetProjectEnvironment("./tests/environment/uwu");
             assertEquals(env, uwu);
         },
     },
