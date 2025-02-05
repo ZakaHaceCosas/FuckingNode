0e31918183432376085c932ad58942d58c7a4ece
Zaka <zakahacecosas@protonmail.com>
2025-02-05 19:41:40 +0100

    (v3) update compat tables to look better

--- src/commands/compat.ts
+++ src/commands/compat.ts
@@ -3,33 +3,39 @@ import { ColorString, LogStuff } from "../functions/io.ts";
 import type { TheCompaterConstructedParams } from "./constructors/command.ts";
 import { APP_URLs } from "../constants.ts";
 
+const indicators = {
+    y: ColorString("Yes", "bright-green"),
+    n: ColorString("No", "red"),
+    p: ColorString("Partial", "orange"),
+};
+
 const featureCompatibility = [
-    { Feature: "Cleanup", NodeJS: "Yes", Deno: "Partial", Bun: "Partial", Go: "Partial", Cargo: "No" },
-    { Feature: "Kickstart", NodeJS: "Yes", Deno: "Yes", Bun: "Yes", Go: "Yes", Cargo: "Yes" },
-    { Feature: "Commit", NodeJS: "Yes", Deno: "Yes", Bun: "Yes", Go: "Partial", Cargo: "Partial" },
-    { Feature: "Release", NodeJS: "npm", Deno: "jsr", Bun: "npm", Go: "No", Cargo: "No" },
+    { Feature: "Cleanup", NodeJS: indicators.y, Deno: indicators.p, Bun: indicators.p, Go: indicators.p, Cargo: indicators.y },
+    { Feature: "Kickstart", NodeJS: indicators.y, Deno: indicators.y, Bun: indicators.y, Go: indicators.y, Cargo: indicators.y },
+    { Feature: "Commit", NodeJS: indicators.y, Deno: indicators.y, Bun: indicators.y, Go: indicators.p, Cargo: indicators.p },
+    { Feature: "Release", NodeJS: "npm", Deno: "jsr", Bun: "npm", Go: indicators.y, Cargo: indicators.y },
 ];
 
 const advancedFeatureCompatibility = [
-    { Option: "Lint", NodeJS: "Yes", Deno: "No", Bun: "Yes", Go: "No", Cargo: "No" },
-    { Option: "Pretty", NodeJS: "Yes", Deno: "Partial", Bun: "Yes", Go: "Yes", Cargo: "Yes (untested)" },
-    { Option: "Destroy", NodeJS: "Yes", Deno: "Yes", Bun: "Yes", Go: "Yes", Cargo: "Yes" },
-    { Option: "Update", NodeJS: "Yes", Deno: "Yes", Bun: "Yes", Go: "Yes", Cargo: "Yes" },
+    { Option: "Lint", NodeJS: indicators.y, Deno: indicators.y, Bun: indicators.y, Go: indicators.y, Cargo: indicators.y },
+    { Option: "Pretty", NodeJS: indicators.y, Deno: indicators.p, Bun: indicators.y, Go: indicators.y, Cargo: indicators.y },
+    { Option: "Destroy", NodeJS: indicators.y, Deno: indicators.y, Bun: indicators.y, Go: indicators.y, Cargo: indicators.y },
+    { Option: "Update", NodeJS: indicators.y, Deno: indicators.y, Bun: indicators.y, Go: indicators.y, Cargo: indicators.y },
 ];
 
 const kickstartCompatibility = [
-    { Feature: "Kickstart itself", NodeJS: "Yes", Deno: "Yes", Bun: "Yes", Go: "Yes", Cargo: "Yes" },
+    { NodeJS: indicators.y, Deno: indicators.y, Bun: indicators.y, Go: indicators.y, Cargo: indicators.y },
 ];
 
 const migrateCompatibility = [
-    { From: "NodeJS", To: "Deno", Supported: "NO" },
-    { From: "NodeJS", To: "Bun", Supported: "YES" },
-    { From: "Deno", To: "NodeJS", Supported: "NO" },
-    { From: "Bun", To: "NodeJS", Supported: "YES" },
-    { From: "------" },
-    { From: "NodeJS / npm", To: "pnpm / yarn", Supported: "YES" },
-    { From: "NodeJS / pnpm", To: "npm / yarn", Supported: "YES" },
-    { From: "NodeJS / yarn", To: "npm / pnpm", Supported: "YES" },
+    { From: "NodeJS", To: "Deno", Supported: indicators.n },
+    { From: "NodeJS", To: "Bun", Supported: indicators.y },
+    { From: "Deno", To: "NodeJS", Supported: indicators.n },
+    { From: "Bun", To: "NodeJS", Supported: indicators.y },
+    { From: "---", To: "---", Supported: "---" },
+    { From: "NodeJS / npm", To: "pnpm / yarn", Supported: indicators.y },
+    { From: "NodeJS / pnpm", To: "npm / yarn", Supported: indicators.y },
+    { From: "NodeJS / yarn", To: "npm / pnpm", Supported: indicators.y },
 ];
 
 export default async function TheCompater(params: TheCompaterConstructedParams) {
@@ -42,8 +48,8 @@ export default async function TheCompater(params: TheCompaterConstructedParams) 
 
     const overallSupport = async () => {
         await LogStuff("OVERALL SUPPORT ---");
-        console.table(featureCompatibility);
-        await LogStuff("\nFor specific compatibility details, run 'compat' followed by any of these: cleaner, kickstart, migrate.");
+        await LogStuff(StringUtils.table(featureCompatibility));
+        await LogStuff("For specific compatibility details, run 'compat' followed by any of these: cleaner, kickstart, migrate.");
         return;
     };
 
@@ -56,19 +62,19 @@ export default async function TheCompater(params: TheCompaterConstructedParams) 
         case "cleaner":
         case "advanced":
             await LogStuff("ADVANCED CLEANER FEATURES SUPPORT ---");
-            console.table(advancedFeatureCompatibility);
+            await LogStuff(StringUtils.table(advancedFeatureCompatibility));
             return;
         case "kickstart":
-            await LogStuff("KICKSTART FEATURES SUPPORT ---");
-            console.table(kickstartCompatibility);
+            await LogStuff("KICKSTART FEATURE SUPPORT ---");
+            await LogStuff(StringUtils.table(kickstartCompatibility));
             // IDEs too, this is simple so i ain't extracting to a constant
-            console.table([
+            await LogStuff(StringUtils.table([
                 { "Supported IDEs": ["VSCode", "VSCodium", "Notepad++", "Sublime", "Emacs", "Atom"] },
-            ]);
+            ]));
             return;
         case "migrate":
             await LogStuff("MIGRATE FEATURES SUPPORT ---");
-            console.table(migrateCompatibility);
+            await LogStuff(StringUtils.table(migrateCompatibility));
             return;
         default:
             await overallSupport();
